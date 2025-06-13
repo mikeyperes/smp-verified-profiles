43dff304857cda2be65779d47f0ba2114b44754a
Your Name <you@example.com>
2025-06-13 21:02:28 +0000

    .mo language bug fix

--- initialization.php
+++ initialization.php
@@ -6,6 +6,8 @@ Author: Michael Peres
 Plugin URI: https://github.com/mikeyperes/smp-verified-profiles
 Description: Verified Profile integration for Scale My Publication systems.
 Version: 3.4.1
+Text Domain: smp-verified-profiles
+Domain Path: /languages
 Author URI: https://michaelperes.com
 GitHub Plugin URI: https://github.com/mikeyperes/smp-verified-profiles
 GitHub Branch: main
