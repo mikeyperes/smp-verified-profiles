d82fbdff2082dffa18c892ae4b46e660b6b20cdd
Your Name <you@example.com>
2025-08-04 07:35:47 +0000

    fixed schema output html injection

--- snippet-inject-schema-on-single-profile.php
+++ snippet-inject-schema-on-single-profile.php
@@ -1,25 +1,25 @@
 <?php namespace smp_verified_profiles;
-function enable_snippet_inject_schema_on_single_profile()
-{
 
-add_action('wp_head', __NAMESPACE__.'\inject_schema_on_single_profile');
+function enable_snippet_inject_schema_on_single_profile() {
+  add_action( 'wp_head', __NAMESPACE__ . '\inject_schema_on_single_profile', 1 );
 }
+add_action( 'init', __NAMESPACE__ . '\enable_snippet_inject_schema_on_single_profile' );
+
 /**
  * Inject schema markup into the head section of a 'profile' post type single view.
  */
-if (!function_exists(__NAMESPACE__ . '\\inject_schema_on_single_profile')) {
-    function inject_schema_on_single_profile() {
-  
-        $verified_profile_settings = get_verified_profile_settings();
-        //|| is_singular($verified_profile_settings["entity"]) 
-        if (is_singular($verified_profile_settings["slug"])) { 
-            global $post;
-            $schema_json = get_field('schema_markup', $post->ID);
-            if ($schema_json) {
-                echo "<script type='application/ld+json'>" . $schema_json . "</script>";
-            }
-        }
+if ( ! function_exists( __NAMESPACE__ . '\\inject_schema_on_single_profile' ) ) {
+  function inject_schema_on_single_profile() {
+    $settings = get_verified_profile_settings();
+    if ( is_singular( $settings['slug'] ) ) {
+      global $post;
+      // 2) false = return raw value without ACF formatting
+      $schema_json = get_field( 'schema_markup', $post->ID, false );
+      if ( $schema_json ) {
+        echo '<script type="application/ld+json">' . $schema_json . '</script>';
+      }
     }
+  }
 } else {
-    write_log("⚠️ Warning: " . __NAMESPACE__ . "\\inject_schema_on_single_profile function is already declared", true);
+  write_log( "⚠️ Warning: " . __NAMESPACE__ . "\\inject_schema_on_single_profile already declared", true );
 }
